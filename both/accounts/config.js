69c9b7d48860e8eae8eed9e508a6478e6f5ba918
Evans Marindany <evans@yangoweb.com.au>
2015-10-12 03:17:37 +0300

    Text change for home page

--- both/accounts/config.js
+++ both/accounts/config.js
@@ -8,16 +8,28 @@ AccountsTemplates.configure({
     focusFirstInput: true,
     enforceEmailVerification: true,
 });
+
+// if (Meteor.isServer){
+//     Meteor.methods({
+//         'validatePhone': function (number) {
+//             var urlRegEx = /^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}/;
+//             return  Match.test(number, urlRegEx);
+//         },
+//     });
+// }
+
 AccountsTemplates.addField({
     _id: 'phone',
     type: 'tel',
-    displayName: "Phone Number",
+    displayName: "Phone e.g 0721123456",
     required: true,
     // func: function (number) {
     //     if (Meteor.isServer){
-    //       if (isValidPhone(number))
-    //           return false; // meaning no error!
-    //       return true; // Validation error!
+    //     //   if (validatePhone(number)){
+    //         Meteor.sendVerificationCode('+254'+number);
+    //           // return false; // meaning no error!
+    //     //   }
+    //     //   return true; // Validation error!
     //     }
     // },
     errStr: 'Invalid Phone number!',
